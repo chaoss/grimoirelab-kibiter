a9aa2f334c78ca3437b9cfcd420e0918a8a70615
Luis Cañas Díaz <lcanas@bitergia.com>
2018-06-07 14:20:57 +0200

    Fix #79 to allow the defaultAppId variable to contain '/'

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -25,7 +25,7 @@ if [ "$1" = 'kibana' ]; then
         fi
 
         if [ "$KIBANA_DEFAULTAPPID" != "" ]; then
-                sed -i "s/^#kibana.defaultAppId.*/kibana.defaultAppId: $KIBANA_DEFAULTAPPID/" /opt/kibana/config/kibana.yml
+                sed -i "s|^#kibana.defaultAppId.*|kibana.defaultAppId: $KIBANA_DEFAULTAPPID|" /opt/kibana/config/kibana.yml
         fi
 
         if [ "$PROJECT_NAME" != "" ]; then
